8c260f547efbbdc134ab940dcdc6aa98e9d06bc4
Agnus290 <tenebris.agnus@yandex.ru>
2022-02-12 19:33:10 +0300

    Наводим красоту. rf24 -> nrf24, дефайны перенесены в отдельный .h

--- stm32f4/nRF24L01_PL/nrf24_lower_api.h
+++ stm32f4/nRF24L01_PL/nrf24_lower_api.h
@@ -1,10 +1,3 @@
-/*
- * nrf24_lower_api.h
- *
- *  Created on: 29 янв. 2022 г.
- *      Author: 1
- */
-
 #ifndef NRF24_LOWER_API_H_
 #define NRF24_LOWER_API_H_
 
@@ -15,104 +8,21 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-// Регистры
-#define RF24_REGADDR_CONFIG			0x00
-#define RF24_REGADDR_EN_AA			0x01
-#define RF24_REGADDR_EN_RXADDR		0x02
-#define RF24_REGADDR_SETUP_AW		0x03
-#define RF24_REGADDR_SETUP_RETR		0x04
-#define RF24_REGADDR_RF_CH			0x05
-#define RF24_REGADDR_RF_SETUP		0x06
-#define RF24_REGADDR_STATUS			0x07
-#define RF24_REGADDR_OBSERVE_TX		0x08
-#define RF24_REGADDR_RPD			0x09
-#define RF24_REGADDR_RX_ADDR_P0		0x0A
-#define RF24_REGADDR_RX_ADDR_P1		0x0B
-#define RF24_REGADDR_RX_ADDR_P2		0x0C
-#define RF24_REGADDR_RX_ADDR_P3		0x0D
-#define RF24_REGADDR_RX_ADDR_P4		0x0E
-#define RF24_REGADDR_RX_ADDR_P5		0x0F
-#define RF24_REGADDR_TX_ADDR		0x10
-#define RF24_REGADDR_RX_PW_P0		0x11
-#define RF24_REGADDR_RX_PW_P1		0x12
-#define RF24_REGADDR_RX_PW_P2		0x13
-#define RF24_REGADDR_RX_PW_P3		0x14
-#define RF24_REGADDR_RX_PW_P4		0x15
-#define RF24_REGADDR_RX_PW_P5		0x16
-#define RF24_REGADDR_FIFO_STATUS	0x17
-#define RF24_REGADDR_DYNPD			0x1C
-#define RF24_REGADDR_FEATURE		0x1D
+#include "nrf24_defs.h"
 
-// Есть данные в RX буферах
-#define RF24_STATUS_RX_DR	(1 << 6)
-// Данные из TX буфера отправлены
-#define RF24_STATUS_TX_DS	(1 << 5)
-// Превышено количество попыток отправки данных
-#define RF24_STATUS_MAX_RT	(1 << 4)
-// Нет данных в TX буферах
-#define RF24_STATUS_TX_FULL	(1 << 0)
-
-
-// Номер пайпа, пакет с которого ожидает выгребания в RX FIFO
-#define RF24_STATUS_RX_P_NO_GET(status_reg) ((status_reg >> 1) & (0x07)) // 0x07 это 111 в двоичной
-
-
-// Прерывание по получению данных
-#define RF24_CONFIG_MASK_RX_DR	(1 << 6)
-// Прерывание по успешной отправке данных
-#define RF24_CONFIG_MASK_TX_DR	(1 << 5)
-// Прерывание по проавлу отправки данных
-#define RF24_CONFIG_MASK_MAX_RT	(1 << 4)
-// Включение CRC (всегда включено, если включены авто-ACK)
-#define RF24_CONFIG_EN_CRC		(1 << 3)
-// Длина CRC (0 - 1 байт, 1 - 2 байта)
-#define RF24_CONFIG_CRCO		(1 << 2)
-// Включение модуля (если 0, то модуль спит)
-#define RF24_CONFIG_PWR_UP		(1 << 1)
-// Упрвавление режимом - приём или передача
-#define RF24_CONFIG_PRIM_RX		(1 << 0)
-
-
-#define RF24_RFSETUP_CONT_WAVE	 (1 << 7)
-#define RF24_RFSETUP_RF_DR_LOW	 (1 << 5)
-#define RF24_RFSETUP_PLL_LOCK	 (1 << 4)
-#define RF24_RFSETUP_RF_DR_HIGH	 (1 << 3)
-#define RF24_RFSETUP_RF_PWR_OFFSET  1
-#define RF24_RFSETUP_RF_PWR_MASK    0x03
-
-
-#define RF24_FEATURE_EN_DPL		(1 << 2)
-#define RF24_FEATURE_EN_ACK_PAY	(1 << 1)
-#define RF24_FEATURE_EN_DYN_ACK	(1 << 0)
-
-
-// Команды
-#define R_REGISTER               0b000 // В конец пишется адрес регистра (5 бит)
-#define W_REGISTER               0b001 // В конец пишется адрес регистра (5 бит)
-#define RF24_R_RX_PAYLOAD        0b01100001
-#define RF24_W_TX_PAYLOAD        0b10100000
-#define RF24_FLUSH_TX	         0b11100001
-#define RF24_FLUSH_RX	         0b11100010
-#define REUSE_TX_PL	             0b11100011
-#define RF24_R_RX_PL_WID         0b01100000
-#define W_ACK_PAYLOAD            0b10101 // В конец пишется номер пайпа (3 бита)
-#define RF24_W_TX_PAYLOAD_NO_ACK 0b10110000
-#define RF24_NOP                 0b11111111
-
-
-// Чтение данных из регистров RF24
+// Чтение данных из регистров NRF24
 /* Аргументы:
    reg_addr - адрес читаемого регистра
    reg_data - указатель на буфер в который кладутся прочитанные данные
    data_size - размер буфера для прочитанных данных */
-void rf24_read_register(uint8_t reg_addr, uint8_t * reg_data, size_t data_size);
+void nrf24_read_register(uint8_t reg_addr, uint8_t * reg_data, size_t data_size);
 
-// Запись данных в регистры RF24
+// Запись данных в регистры NRF24
 /* Аргументы:
    reg_addr - адрес записываемого регистра
    reg_data - указатель на буфер с записываемыми данными
    data_size - размер буфера с записываемыми данными */
-void rf24_write_register(uint8_t reg_addr, const uint8_t * reg_data, size_t data_size);
+void nrf24_write_register(uint8_t reg_addr, const uint8_t * reg_data, size_t data_size);
 
 // Чтение полученного пакета
 /* Аргументы:
@@ -120,9 +30,9 @@ void rf24_write_register(uint8_t reg_addr, const uint8_t * reg_data, size_t data
    payload_buffer_size - размер буфера payload_buffer.
 
    Если полученный пакет не влезает в payload_buffer, то будет записано только то, что влезло
-   пакет удалется из RF24 при чтении. А  максимум он может быть 32 байта.
+   пакет удалется из NRF24 при чтении. А  максимум он может быть 32 байта.
    Для получения длины полученного пакета можно использовать */
-void rf24_read_rx_payload(uint8_t * payload_buffer, size_t payload_buffer_size);
+void nrf24_read_rx_payload(uint8_t * payload_buffer, size_t payload_buffer_size);
 
 // Запись пакета для отправки
 /* Аргументы:
@@ -131,36 +41,36 @@ void rf24_read_rx_payload(uint8_t * payload_buffer, size_t payload_buffer_size);
    use_ack - нужно ли использовать ACK для этого пакета?
 
    Эта функция реализует две команды: W_TX_PAYLOAD_NOACK и W_TX_PAYLOAD */
-void rf24_write_tx_payload(const uint8_t * payload_buffer, size_t payload_size, bool use_ack);
+void nrf24_write_tx_payload(const uint8_t * payload_buffer, size_t payload_size, bool use_ack);
 
 // Сброс всех данных из буферов RF на отправку
-void rf24_flush_tx(void);
+void nrf24_flush_tx(void);
 
-// Сброс всех полученных rf24 пакетов
-void rf24_flush_rx(void);
+// Сброс всех полученных nrf24 пакетов
+void nrf24_flush_rx(void);
 
 // Повтор предыдущего отправляемого пакета
-void rf24_ruse_tx_pl(void);
+void nrf24_ruse_tx_pl(void);
 
 // Получает из радио размер полученного пакета
 /* Документация говорит, что нужно сделать flush_rx (или он был сделан?)
    Если прочитался размер больше 32ух байт.
    Предполагаю что для пустого FIFO прочитается ноль? */
-void rf24_get_rx_payload_size(uint8_t * payload_size);
+void nrf24_get_rx_payload_size(uint8_t * payload_size);
 
 // Запись пакета для отправки вместе с очередным ACK пакетом
 /* Аргументы:
    payload - указатель на буфер с отправляемым пакетом
    payload_size - размер отправляемого пакета в буфере payload_buffer
    pipe - номер пайпа */
-void rf24_write_ack_payload(const uint8_t * payload, size_t payload_size, uint8_t pipe);
+void nrf24_write_ack_payload(const uint8_t * payload, size_t payload_size, uint8_t pipe);
 
 // Получение статуса устройства
-void rf24_get_status(uint8_t * status);
+void nrf24_get_status(uint8_t * status);
 
 // Управление CE пином модуля.
 /* onoff: значение true - CE прижат к полу; false - CE поднят к 1 */
-void rf24_ce_activate(bool onoff);
+void nrf24_ce_activate(bool onoff);
 
 #endif /* HAL_SPI_MODULE_ENABLED */
 #endif /* NRF24_LOWER_API_H_ */
--- stm32f4/nRF24L01_PL/src/nrf24_lower_api.c
+++ stm32f4/nRF24L01_PL/src/nrf24_lower_api.c
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #include "../nrf24_lower_api.h"
+#include "../nrf24_defs.h"
 
 extern SPI_HandleTypeDef hspi2;
 
@@ -16,7 +17,7 @@ extern SPI_HandleTypeDef hspi2;
 #define CS_PORT GPIOA
 #define CS_PIN GPIO_PIN_0
 
-static void _rf24_CS(bool mode)
+static void _nrf24_CS(bool mode)
 {
 	if (mode)
 	{
@@ -32,9 +33,9 @@ static void _rf24_CS(bool mode)
 	}
 }
 
-void rf24_read_register(uint8_t reg_addr, uint8_t * reg_data, size_t data_size)
+void nrf24_read_register(uint8_t reg_addr, uint8_t * reg_data, size_t data_size)
 {
-	_rf24_CS(true);
+	_nrf24_CS(true);
 
 	// Добавляем в 5 битов адреса еще 3 бита для чтения из этого регистра
 	reg_addr = reg_addr & ~((1 << 5) | (1 << 6) | (1 << 7));
@@ -43,12 +44,12 @@ void rf24_read_register(uint8_t reg_addr, uint8_t * reg_data, size_t data_size)
 	HAL_SPI_Transmit(&hspi2, &reg_addr, 1, HAL_MAX_DELAY);
 	HAL_SPI_Receive(&hspi2, reg_data, data_size, HAL_MAX_DELAY);
 
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_write_register(uint8_t reg_addr, const uint8_t * reg_data, size_t data_size)
+void nrf24_write_register(uint8_t reg_addr, const uint8_t * reg_data, size_t data_size)
 {
-	_rf24_CS(true);
+	_nrf24_CS(true);
 
 	// Добавляем в 5 битов адреса еще 3 бита для записи в регистр
 	reg_addr = (reg_addr & ~((1 << 6) | (1 << 7))) | (1 << 5);
@@ -57,15 +58,15 @@ void rf24_write_register(uint8_t reg_addr, const uint8_t * reg_data, size_t data
 	HAL_SPI_Transmit(&hspi2, &reg_addr, 1, HAL_MAX_DELAY);
 	HAL_SPI_Transmit(&hspi2, (uint8_t*)reg_data, data_size, HAL_MAX_DELAY);
 
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_read_rx_payload(uint8_t * payload_buffer, size_t payload_buffer_size)
+void nrf24_read_rx_payload(uint8_t * payload_buffer, size_t payload_buffer_size)
 {
-	uint8_t command = RF24_R_RX_PAYLOAD;
+	uint8_t command = NRF24_R_RX_PAYLOAD;
 	uint8_t payload_size = 0;
     // Считываем размер данных
-	rf24_get_rx_payload_size(&payload_size);
+	nrf24_get_rx_payload_size(&payload_size);
 	// Если размер данных от 1 до 32, то читаем данные
 	if (payload_size > 0 || payload_size < 32)
 	{
@@ -74,96 +75,96 @@ void rf24_read_rx_payload(uint8_t * payload_buffer, size_t payload_buffer_size)
 		{
 			payload_size = payload_buffer_size;
 		}
-		_rf24_CS(true);
+		_nrf24_CS(true);
 		// Читаем данные
 		HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
 		HAL_SPI_Receive(&hspi2, payload_buffer, payload_size, HAL_MAX_DELAY);
-		_rf24_CS(false);
+		_nrf24_CS(false);
 	}
 }
 
-void rf24_write_tx_payload(const uint8_t * payload_buffer, size_t payload_size, bool use_ack)
+void nrf24_write_tx_payload(const uint8_t * payload_buffer, size_t payload_size, bool use_ack)
 {
 	uint8_t command;
-	_rf24_CS(true);
+	_nrf24_CS(true);
 	// Учитываем использование ack
 	if (use_ack)
 	{
-		command = RF24_W_TX_PAYLOAD;
+		command = NRF24_W_TX_PAYLOAD;
 	}
 	else
 	{
-		command = RF24_W_TX_PAYLOAD_NO_ACK;
+		command = NRF24_W_TX_PAYLOAD_NO_ACK;
 	}
 	// Передаем данные
 	HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
 	HAL_SPI_Transmit(&hspi2, (uint8_t*)payload_buffer, payload_size, HAL_MAX_DELAY);
 
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_flush_rx(void)
+void nrf24_flush_rx(void)
 {
-	uint8_t command = RF24_FLUSH_RX;
-	_rf24_CS(true);
+	uint8_t command = NRF24_FLUSH_RX;
+	_nrf24_CS(true);
 	HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_flush_tx(void)
+void nrf24_flush_tx(void)
 {
-	uint8_t command = RF24_FLUSH_TX;
-	_rf24_CS(true);
+	uint8_t command = NRF24_FLUSH_TX;
+	_nrf24_CS(true);
 	HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
 // Повтор предыдущего отправляемого пакета
-void rf24_ruse_tx_pl(void)
+void nrf24_ruse_tx_pl(void)
 {
 	uint8_t command = REUSE_TX_PL;
-	_rf24_CS(true);
+	_nrf24_CS(true);
 	HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_get_rx_payload_size(uint8_t * payload_size)
+void nrf24_get_rx_payload_size(uint8_t * payload_size)
 {
-	uint8_t command = RF24_R_RX_PL_WID;
-	_rf24_CS(true);
+	uint8_t command = NRF24_R_RX_PL_WID;
+	_nrf24_CS(true);
 	HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
 	HAL_SPI_Receive(&hspi2, payload_size, 1, HAL_MAX_DELAY);
-	_rf24_CS(false);
+	_nrf24_CS(false);
 	// Если размер данных больше 32, то отчищаем rx буфер и зануляем его
     if (*payload_size > 32)
     {
-    	rf24_flush_rx();
+    	nrf24_flush_rx();
     	payload_size = 0;
     }
 }
 
 // Запись пакета для отправки вместе с очередным ACK пакетом
-void rf24_write_ack_payload(const uint8_t * payload, size_t payload_size, uint8_t pipe)
+void nrf24_write_ack_payload(const uint8_t * payload, size_t payload_size, uint8_t pipe)
 {
 	uint8_t command = (W_ACK_PAYLOAD << 3) | (pipe & 0x07);
-	_rf24_CS(true);
+	_nrf24_CS(true);
 
 	// Передаем данные
 	HAL_SPI_Transmit(&hspi2, &command, 1, HAL_MAX_DELAY);
 	HAL_SPI_Transmit(&hspi2, (uint8_t*)payload, payload_size, HAL_MAX_DELAY);
 
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_get_status(uint8_t * status)
+void nrf24_get_status(uint8_t * status)
 {
-	uint8_t command = RF24_NOP;
-	_rf24_CS(true);
+	uint8_t command = NRF24_NOP;
+	_nrf24_CS(true);
 	HAL_SPI_TransmitReceive(&hspi2, &command, status, 1, HAL_MAX_DELAY);
-	_rf24_CS(false);
+	_nrf24_CS(false);
 }
 
-void rf24_ce_activate(bool onoff)
+void nrf24_ce_activate(bool onoff)
 {
 	if (onoff)
 	{
