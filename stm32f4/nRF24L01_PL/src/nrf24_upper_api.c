a8e265b9a2c4d02ff5b1509476fd1fb641eeb3cb
@man-GO-228 <t.ozhigow@gmail.com>
2022-03-02 16:24:36 +0300

    Дописали настройки динамической длинный пейлоада для пайпов

--- stm32f4/nRF24L01_PL/src/nrf24_upper_api.c
+++ stm32f4/nRF24L01_PL/src/nrf24_upper_api.c
@@ -100,33 +100,48 @@ int nrf24_pipe_rx_start(uint8_t pipe_no, const nrf24_pipe_config_t * config)
 	en_rxaddr |= (1 << pipe_no);
 	nrf24_write_register(NRF24_REGADDR_EN_RXADDR, &en_rxaddr, 1);
 
+	uint8_t rx_pw = 0;
 	switch (pipe_no)
 	{
 	case 0:
 		nrf24_write_register(NRF24_REGADDR_RX_ADDR_P0, (uint8_t *)(&config->address), 5);
-		nrf24_write_register(NRF24_REGADDR_RX_PW_P0, (uint8_t *)&config->payload_size, 1);
+		rx_pw = NRF24_REGADDR_RX_PW_P0;
 		break;
 	case 1:
 		nrf24_write_register(NRF24_REGADDR_RX_ADDR_P1, (uint8_t *)(&config->address), 5);
-		nrf24_write_register(NRF24_REGADDR_RX_PW_P1, (uint8_t *)&config->payload_size, 1);
+		rx_pw = NRF24_REGADDR_RX_PW_P1;
 		break;
 	case 2:
 		nrf24_write_register(NRF24_REGADDR_RX_ADDR_P2, ((uint8_t *)(&config->address) + 4), 1);
-		nrf24_write_register(NRF24_REGADDR_RX_PW_P2, (uint8_t *)&config->payload_size, 1);
+		rx_pw = NRF24_REGADDR_RX_PW_P2;
 		break;
 	case 3:
 		nrf24_write_register(NRF24_REGADDR_RX_ADDR_P3, ((uint8_t *)(&config->address) + 4), 1);
-		nrf24_write_register(NRF24_REGADDR_RX_PW_P3, (uint8_t *)&config->payload_size, 1);
+		rx_pw = NRF24_REGADDR_RX_PW_P3;
 		break;
 	case 4:
 		nrf24_write_register(NRF24_REGADDR_RX_ADDR_P4, ((uint8_t *)(&config->address) + 4), 1);
-		nrf24_write_register(NRF24_REGADDR_RX_PW_P4, (uint8_t *)&config->payload_size, 1);
+		rx_pw = NRF24_REGADDR_RX_PW_P4;
 		break;
 	case 5:
 		nrf24_write_register(NRF24_REGADDR_RX_ADDR_P5, ((uint8_t *)(&config->address) + 4), 1);
-		nrf24_write_register(NRF24_REGADDR_RX_PW_P5, (uint8_t *)&config->payload_size, 1);
+		rx_pw = NRF24_REGADDR_RX_PW_P5;
 		break;
 	}
+
+	uint8_t dynpd = 0;
+	nrf24_read_register(NRF24_REGADDR_DYNPD, &dynpd, 1);
+	if (config->payload_size >= 0)
+	{
+		nrf24_write_register(rx_pw, (uint8_t *)(&config->payload_size), 1);
+		dynpd &= ~(1 << pipe_no);
+	}
+	else
+	{
+		dynpd |= (1 << pipe_no);
+	}
+	nrf24_write_register(NRF24_REGADDR_DYNPD, &dynpd, 1);
+
 	return 0;
 }
 
