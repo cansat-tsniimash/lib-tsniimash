fab8ac0e2ffd3a0f8ededbf35541aaa8ba1beb3c
Jly4e3apHbiu <pasha0000407@mail.ru>
2022-03-19 20:07:51 +0300

    написали комменты на драйвер гпс

--- stm32f4/ATGM336H/nmea_gps.h
+++ stm32f4/ATGM336H/nmea_gps.h
@@ -11,15 +11,29 @@
 #include <stdint.h>
 
 int gps_init();
-
+/*
+ * принимает байт с гпски и записывает во внутренний буфер
+ */
 int gps_push_byte(uint8_t byte);
 
-
+/*
+ * обрабатывает внутренний буфер и парсит его
+ */
 int gps_work();
-
-
+/*
+ *   функция записывает координаты в переменные по указателю lat_, lon_ и alt_ и возвращает  0
+ *   cookie - количество измерений
+ */
 int gps_get_coords(int64_t * cookie, float * lat_, float * lon_, float * alt_);
+/*
+ * функция записывает высоту в переменную по указателю alt и возвращает 0
+ * cookie - количество измерений
+ */
 int gps_get_alt(int64_t * cookie, float * alt);
+/*
+ *  функция записывает время в секундах(по указателю в time_s) и в микросекундах(по указателю в time_us) и возвращает 0
+ *  cookie - количество измерений
+ */
 int gps_get_time(int64_t * cookie, uint64_t * time_s, uint32_t * time_us);
 
 
