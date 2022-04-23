323e46f4e066de7ae6d4cdfa46d7bda5cb081911
@man-GO-228 <t.ozhigow@gmail.com>
2022-04-23 18:14:06 +0300

    Перенесли БМЕ

---
+++ stm32f1/BME280/DriverForBME280.h
@@ -0,0 +1,53 @@
+/*
+ * Данные два рукотворные файлы программиста П (DBME280.c, DBME280.h) являются всего лишь прокладками, соединяющими, упро-
+ * щающими и сокращающими остальные огромные строки кода из файлов папки BME280 и её подпапки src, которые написаны
+ * программистами компании Bosh.
+ */
+
+
+
+
+
+#include <stm32f1xx_hal.h>
+
+
+#ifdef HAL_SPI_MODULE_ENABLED
+
+
+#include <stdio.h>
+#include "bme280.h"
+
+
+
+//Структура, содержащая в себе информацию о порте и пине подключения SPI CS
+struct bme_spi_intf
+{
+	GPIO_TypeDef  *GPIO_Port;
+	uint16_t GPIO_Pin;
+	SPI_HandleTypeDef* spi;
+};
+
+
+
+struct bme_spi_intf_sr
+{
+	int sp_pin;
+	SPI_HandleTypeDef* spi;
+	//Shift reg device
+};
+
+
+
+/*
+ * Создаёт дефолтные настройки, которые написал я и немцы, которые придумали датчик
+ * spi_intf - настройка интерфейса SPI
+ */
+void bme_init_default( struct bme280_dev *bme, struct bme_spi_intf* spi_intf);
+
+// Чтение данных с БМЕ280, может возвращать давление, влажность и температуру
+struct bme280_data bme_read_data(struct bme280_dev *bme);
+
+
+
+void bme_init_default_sr( struct bme280_dev *bme, struct bme_spi_intf_sr* spi_intf);
+#endif
---
+++ stm32f1/BME280/src/DriverForBME280.c
@@ -0,0 +1,193 @@
+#include <stm32f1xx_hal.h>
+
+
+#ifdef HAL_SPI_MODULE_ENABLED
+
+
+#include <stdio.h>
+#include "..\bme280.h"
+#include "..\DriverForBME280.h"
+
+
+
+
+
+
+
+
+
+
+static BME280_INTF_RET_TYPE bme_spi_read(
+		uint8_t reg_addr, uint8_t * data, uint32_t data_len, void *intf_ptr
+)
+{
+
+
+	struct bme_spi_intf* spi_intf = intf_ptr;
+
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_RESET);
+	reg_addr |= (1 << 7);
+	HAL_SPI_Transmit(spi_intf->spi, &reg_addr, 1, HAL_MAX_DELAY);
+	HAL_SPI_Receive(spi_intf->spi, data, data_len, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_SET);
+
+	return 0;
+}
+
+
+static BME280_INTF_RET_TYPE bme_spi_write(
+		uint8_t reg_addr, const uint8_t * data, uint32_t data_len, void *intf_ptr
+)
+{
+
+	struct bme_spi_intf* spi_intf = intf_ptr;
+
+
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_RESET);
+	reg_addr &= ~(1 << 7);
+	HAL_SPI_Transmit(spi_intf->spi, &reg_addr, 1, HAL_MAX_DELAY);
+	HAL_SPI_Transmit(spi_intf->spi, (uint8_t*)data, data_len, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_SET);
+
+	return 0;
+}
+
+
+static void bme_delay_us(uint32_t period, void *intf_ptr)
+{
+	if (period < 1000)
+		period = 1;
+	else
+		period = period / 1000;
+
+	HAL_Delay(period);
+}
+
+
+
+static BME280_INTF_RET_TYPE bme_spi_read_sr(
+		uint8_t reg_addr, uint8_t * data, uint32_t data_len, void *intf_ptr
+)
+{
+
+
+	struct bme_spi_intf* spi_intf = intf_ptr;
+
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_RESET);
+	reg_addr |= (1 << 7);
+	HAL_SPI_Transmit(spi_intf->spi, &reg_addr, 1, HAL_MAX_DELAY);
+	HAL_SPI_Receive(spi_intf->spi, data, data_len, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_SET);
+
+	return 0;
+}
+
+
+static BME280_INTF_RET_TYPE bme_spi_write_sr(
+		uint8_t reg_addr, const uint8_t * data, uint32_t data_len, void *intf_ptr
+)
+{
+
+	struct bme_spi_intf* spi_intf = intf_ptr;
+
+
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_RESET);
+	reg_addr &= ~(1 << 7);
+	HAL_SPI_Transmit(spi_intf->spi, &reg_addr, 1, HAL_MAX_DELAY);
+	HAL_SPI_Transmit(spi_intf->spi, (uint8_t*)data, data_len, HAL_MAX_DELAY);
+	HAL_GPIO_WritePin(spi_intf->GPIO_Port, spi_intf->GPIO_Pin, GPIO_PIN_SET);
+
+	return 0;
+}
+
+
+static void bme_delay_us_sr(uint32_t period, void *intf_ptr)
+{
+	if (period < 1000)
+		period = 1;
+	else
+		period = period / 1000;
+
+	HAL_Delay(period);
+}
+
+
+
+
+
+
+void bme_init_default( struct bme280_dev *bme, struct bme_spi_intf* spi_intf)
+{
+	// Настройка bme280 =-=-=-=-=-=-=-=-=-=-=-=-
+	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+	(*bme).intf_ptr = spi_intf;
+	(*bme).intf = BME280_SPI_INTF;
+	(*bme).read = bme_spi_read_sr;
+	(*bme).write = bme_spi_write_sr;
+	(*bme).delay_us = bme_delay_us_sr;
+
+    bme280_soft_reset(bme);
+
+
+	bme280_init(bme);
+
+
+	(*bme).settings.osr_h = BME280_OVERSAMPLING_1X;
+	(*bme).settings.osr_p = BME280_OVERSAMPLING_16X;
+	(*bme).settings.osr_t = BME280_OVERSAMPLING_2X;
+	(*bme).settings.filter = BME280_FILTER_COEFF_16;
+	(*bme).settings.standby_time = BME280_STANDBY_TIME_0_5_MS;
+
+	uint8_t settings_sel;
+	settings_sel = BME280_OSR_PRESS_SEL;
+	settings_sel |= BME280_OSR_TEMP_SEL;
+	settings_sel |= BME280_OSR_HUM_SEL;
+	settings_sel |= BME280_FILTER_SEL;
+	settings_sel |= BME280_STANDBY_SEL;
+	 bme280_set_sensor_settings(settings_sel, bme);
+	 bme280_set_sensor_mode(BME280_NORMAL_MODE, bme);
+}
+
+
+
+void bme_init_default_sr( struct bme280_dev *bme, struct bme_spi_intf_sr* spi_intf)
+{
+	// Настройка bme280 =-=-=-=-=-=-=-=-=-=-=-=-
+	// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+	(*bme).intf_ptr = spi_intf;
+	(*bme).intf = BME280_SPI_INTF;
+	(*bme).read = bme_spi_read;
+	(*bme).write = bme_spi_write;
+	(*bme).delay_us = bme_delay_us;
+
+    bme280_soft_reset(bme);
+
+
+	bme280_init(bme);
+
+
+	(*bme).settings.osr_h = BME280_OVERSAMPLING_1X;
+	(*bme).settings.osr_p = BME280_OVERSAMPLING_16X;
+	(*bme).settings.osr_t = BME280_OVERSAMPLING_2X;
+	(*bme).settings.filter = BME280_FILTER_COEFF_16;
+	(*bme).settings.standby_time = BME280_STANDBY_TIME_0_5_MS;
+
+	uint8_t settings_sel;
+	settings_sel = BME280_OSR_PRESS_SEL;
+	settings_sel |= BME280_OSR_TEMP_SEL;
+	settings_sel |= BME280_OSR_HUM_SEL;
+	settings_sel |= BME280_FILTER_SEL;
+	settings_sel |= BME280_STANDBY_SEL;
+	 bme280_set_sensor_settings(settings_sel, bme);
+	 bme280_set_sensor_mode(BME280_NORMAL_MODE, bme);
+}
+struct bme280_data bme_read_data(struct bme280_dev *bme)
+{
+      	// Чтение данные из bme280
+	struct bme280_data comp_data;
+		 bme280_get_sensor_data(BME280_ALL, &comp_data, bme);
+		 return comp_data;
+}
+#endif
